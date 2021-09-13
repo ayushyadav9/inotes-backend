755653634dc201d5e2602fc811a233e2b92a80c2
Ayush Yadav <ayushyadav935@gmail.com>
2021-09-13 14:20:24 +0530

    .

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+var cors = require('cors')
+
+
+const connectToMongo = require("./db")
+const app = express()
+const port = process.env.PORT || 5000
+
+connectToMongo();
+
+app.use(cors())
+app.use(express.json())
+
+//Routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'))
+}
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
